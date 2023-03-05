ab861ce2f20d02fd47cc67b1a796249bdebdaaa1
Mih1rGupt4 <mihirgupta422@gmail.com>
2023-03-05 13:25:18 +0530

    basic ui

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/resources/auth_methods.dart';
 import 'package:zoom_clone/widgets/custom_button.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -9,6 +10,8 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final AuthMethods _authMethods = AuthMethods();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +22,20 @@ class _LoginScreenState extends State<LoginScreen> {
             fontSize: 24,
             fontWeight: FontWeight.bold,
           ),),
-          Image.asset('assets/images/onboarding.jpg'),
-          CustomButtom(text: "Login"),
-          
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 38),
+            child: Image.asset('assets/images/onboarding.jpg'),
+          ),
+          CustomButtom(
+            text: "Google Sign in",
+            onPressed: () async {
+              bool res = await _authMethods.signInWithGoogle(context);
+              if(res) {
+                Navigator.pushNamed(context, '/home');
+              }
+            },
+          ),
+           
         ],
       ),
     );
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/utils/colors.dart';
 
 class CustomButtom extends StatelessWidget {
   final String text;
+  final VoidCallback onPressed;
   CustomButtom({
     super.key,
     required this.text,
+    required this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: (){}, 
-      child: Text(text,
-        style: TextStyle(
-          fontSize: 16
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: buttonColor,
+          minimumSize: const Size(double.infinity, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: const BorderSide(color: buttonColor)
+          )
+        ),
+        onPressed: onPressed, 
+        child: Text(text,
+          style: TextStyle(
+            fontSize: 16
+          ),
         ),
       ),
     );
